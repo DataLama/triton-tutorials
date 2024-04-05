0713c813b9ff27797e266bba20b22f06e03a7973
datalama <kdw4521@gmail.com>
2024-04-05 10:31:15 +0000

    update the code

--- tutorials/basic/2_vector_add.py
+++ tutorials/basic/2_vector_add.py
@@ -1,5 +1,38 @@
+import torch
+import triton
+import triton.language as tl
 
+@triton.jit
+def add_kernel(x_ptr, y_ptr, z_ptr, size, block_size: tl.constexpr):
+    pid = tl.program_id(0)
+    offsets = tl.arange(0, block_size) + pid * block_size 
+    mask = offsets < size
+    
+    x = tl.load(x_ptr + offsets, mask)
+    y = tl.load(y_ptr + offsets, mask)
+    
+    z = x + y
+    
+    tl.store(z_ptr + offsets, mask)
+    
+    
+def add (x, y):
+    z = torch.empty_like(x, device='cuda')
+    size = z.numel()
+    
+    def grid(meta):
+        return (triton.cdiv(size, meta["block_size"]),)
+    
+    add_kernel[grid](x, y, z , size, 1024)
+    
+    return z
 
-# 데이터 로드
-# 성능 이유로 데이터를 불러오는 크기는 2의 거듭 제곱
-# tl.load 
+size = 2 ** 16
+
+x = torch.randn(size, device="cuda")
+y = torch.randn(size, device="cuda")
+
+a = add(x, y)
+b = x + y
+
+assert torch.allclose(a, b)
